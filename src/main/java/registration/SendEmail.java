30422ba1fc6ae428c10bff4788d4a39b10e6db31
Prince Obiuto <152011741+Prince-Obiuto@users.noreply.github.com>
2024-12-01 19:48:53 +0100

    launch ready

--- src/main/java/registration/SendEmail.java
+++ src/main/java/registration/SendEmail.java
@@ -1,7 +1,5 @@
 package registration;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
